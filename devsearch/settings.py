aec0a4ff22e8a30430dcd2bf57edf4a1b26767f8
Lavansh <aroralavit1810@gmail.com>
2021-11-10 15:48:13 +0530

    Added url path in urls.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -147,4 +147,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
