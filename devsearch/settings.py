b077e98ce276f64ef33287440de58e3444b7361e
Lavansh <aroralavit1810@gmail.com>
2021-11-10 17:05:02 +0530

    Added django-heroku

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,5 +167,3 @@ LOGGING = {
     },
 }
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
