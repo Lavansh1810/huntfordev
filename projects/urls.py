28a184fe8416d6c50c5c14d3c0ecef09bed1ef9d
Lavansh <aroralavit1810@gmail.com>
2021-11-27 15:40:10 +0530

    Final try

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.projects, name="projects"),
@@ -7,4 +10,10 @@ urlpatterns = [
     path('create_project/', views.create_project, name="create_project"),
     path('update_project/<str:pk>/', views.update_project, name="update_project"),
     path('delete_project/<str:pk>/', views.delete_project, name="delete_project"),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': 
+          settings.MEDIA_ROOT}),       
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': 
+    settings.STATIC_ROOT}), 
+    
 ]
