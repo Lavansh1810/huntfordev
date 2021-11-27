5be52080ec2df6bdc498b4daeddb6489fd8fa891
Lavansh <aroralavit1810@gmail.com>
2021-11-27 15:34:44 +0530

    Final try

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('login/', views.loginUser, name='login'),
@@ -16,6 +19,11 @@ urlpatterns = [
 
     path('inbox/', views.inbox, name="inbox"),
     path('message/<str:pk>/', views.viewMessage, name="message"),
-    path('create-message/<str:pk>/', views.createMessage, name="create-message")
+    path('create-message/<str:pk>/', views.createMessage, name="create-message"),
 
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': 
+          settings.MEDIA_ROOT}),       
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': 
+    settings.STATIC_ROOT}), 
 ]
